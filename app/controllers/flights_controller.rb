37f73e08f35bf221f642411dee76071441456f1e
Matt Rangel <matt333three@aol.com>
2023-11-14 19:47:45 -0600

    Create search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,6 @@
 class FlightsController < ApplicationController
+  def index
+    @flights = []
+    @airport_options = Airport.all.pluck(:text_code, :id)
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :departing_airport, class_name: "Airport"
   belongs_to :arriving_airport, class_name: "Airport"
+
+  def takeoff_string
+    self.takeoff.strftime("%Y-%m-%d")
+  end
+
+  def self.takeoff_strings
+    self.order(:takeoff).map(&:takeoff_string).uniq
+  end
 end
